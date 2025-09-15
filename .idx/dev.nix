9d2de91010787c5cf1772c7c1ed804b95bfd2708
clement.delille <clement.delille@converteo.com>
2025-09-15 15:06:59 +0000

    feat: Initialisation de l'environnement de développement Nix
    Ce commit introduit la configuration de l'environnement de développement
    basé sur Nix, garantissant une reproductibilité parfaite entre les
    développeurs.
    
    L'environnement dev.nix a été peuplé avec l'ensemble des paquets
    Python nécessaires au fonctionnement de l'application, notamment :
    
    - Interface Web : Streamlit
    - Data Science : Pandas, NumPy
    - Bases de données : SQLAlchemy, psycopg2
    - APIs Google : google-generativeai, google-api-python-client
    - Visualisation : Altair, Plotly
    
    Cette configuration permet de lancer l'application directement avec idx up
    et fournit un shell de développement complet.

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -103,6 +103,8 @@
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       # "vscodevim.vim"
+      "ms-python.debugpy"
+      "ms-python.python"
     ];
     previews = {
       enable = true;
